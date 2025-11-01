86829b51d3c4ed0d3a6fe3307fe70292ef4c09c9
erickvenom <prevenakhtar@mail.com>
2025-11-01 17:37:24 +0000

    corrected query for propertery rating greater than 4

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,24 +1,25 @@
 SELECT
     p.property_id,
-    p.name AS property_name,
+    p.name          AS property_name,
     p.location,
     p.price_per_night,
-    subquery.avg_rating
-FROM
-    Property p
-JOIN (
-    SELECT
-        property_id,
-        AVG(rating) AS avg_rating
-    FROM
-        Reviews
-    GROUP BY
-        property_id
-    HAVING
-        AVG(rating) > 4.0
-) AS subquery ON p.property_id = subquery.property_id
-ORDER BY
-    subquery.avg_rating DESC;
+    (
+        SELECT AVG(r.rating)
+        FROM Reviews r
+        WHERE r.property_id = p.property_id
+        GROUP BY r.property_id
+        HAVING AVG(r.rating) > 4.0
+    )               AS avg_rating
+FROM Property p
+WHERE
+    (
+        SELECT AVG(r.rating)
+        FROM Reviews r
+        WHERE r.property_id = p.property_id
+        GROUP BY r.property_id
+        HAVING AVG(r.rating) > 4.0
+    ) IS NOT NULL
+ORDER BY avg_rating DESC;
 
 
 SELECT
